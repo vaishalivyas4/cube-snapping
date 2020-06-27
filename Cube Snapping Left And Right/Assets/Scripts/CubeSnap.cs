0c46b1b2f40792342ab5f7de5d10269fde86bfd4
Vaishali Vyas <vaishalivyas4@users.noreply.github.com>
2020-06-27 17:28:36 +0530

    Commit#1

---
+++ Cube Snapping Left And Right/Assets/Scripts/CubeSnap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSnap : MonoBehaviour
+{
+    int movement = 3;
+  
+    void Update()
+    {
+        hMovement();
+    }
+
+    void hMovement()
+    {
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-movement, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(movement, 0, 0);
+        }
+
+        movement = Mathf.Clamp(movement, -3, 3);
+    }
+}
